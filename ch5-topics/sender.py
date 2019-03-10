8ac9f82ab3fd5f9d7a2937d8321d66b05e83b967
ayaohsu <ayao780607@gmail.com>
2019-03-10 18:38:31 -0400

    Make if concise

--- ch5-topics/sender.py
+++ ch5-topics/sender.py
@@ -7,9 +7,8 @@ channel = connection.channel()
 channel.exchange_declare(exchange='topic_exchange', 
                          exchange_type='topic')
 
-topic = "anonymous.info"
-if len(sys.argv) >= 2:
-    topic = sys.argv[1]
+topic = sys.argv[1] if len(sys.argv) >= 2 else "anonymous.info"
+
 message = ' '.join(sys.argv[2:]) or "Hello World!"
 
 channel.basic_publish(exchange='topic_exchange',
